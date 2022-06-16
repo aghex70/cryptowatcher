330b49d0384f230ae8d52fc27233580d815168a2
Alberto García Pérez <alberto.garcia@unnax.com>
2022-06-16 22:26:55 +0200

    Added cwapi submodule

--- cwapi/infrastructure/server/server.go
+++
@@ -1,6 +0,0 @@
-package server
-
-type Server interface {
-	StartServer() error
-	StopServer() error
-}
--- cwapi/internal/errors/errors.go
+++
@@ -1,9 +0,0 @@
-package errors
-
-import (
-	"errors"
-)
-
-var (
-	ErrInvalidUsers = errors.New("invalid trade: Buyer and Seller are the same")
-)
